3048f0fdd1a3afbf8d582d25386e31ff0306037f
vorogurcov <vania.frol2010@yandex.ru>
2024-07-17 20:51:31 +0300

    Change Docs

--- Work/prog.py
+++ Work/prog.py
@@ -1,9 +1,30 @@
+import stock
 from stock import Stock
 
+def FuckDown(n):
+  while (n > 0):
+    yield ('FUCK OFF ' + str(n))
+    n-=2
+
+class Player:
+  def __init__(self,name,hp,objects):
+    self.name = name
+    self.hp = hp
+    self.objects = list(objects)
+  def __iter__(self):
+    return self.objects.__iter__()	
+
 def main():
-  goog = Stock('GOOG',100,490.10)
-  goog.cost()
-  goog.sell(25)
-  goog.cost()
+  player = Player('IVAN',100,['penis','huenis','senis'])
+  ig = player.__iter__()
+  while True:
+    try:
+      print(next(ig))
+    except StopIteration:
+      break
+
+  for i in FuckDown(10):
+    print(i + str(6))
+  
 if __name__ == '__main__':
   main()
